89b3425a9f92986f1392bf1a728e1ff10f11706f
huisongxiaomanong <976226535@qq.com>
2019-03-04 18:22:15 +0800

    提交一

---
+++ gittoday/src/main/java/demo/First.java
@@ -0,0 +1,13 @@
+package demo;
+
+/**
+ * @project:today
+ * @description:helloword
+ * @author:刘飞跃
+ * @create:2019年03月04日18点20
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("helloword");
+    }
+}
