2c5d0f37f3388bfb5d45e11870b1e44ab2fc1e44
huisongxiaomanong <976226535@qq.com>
2019-03-04 18:58:21 +0800

    提交dev

---
+++ gittoday/src/main/java/demo/Second.java
@@ -0,0 +1,13 @@
+package demo;
+
+/**
+ * @project:today
+ * @description:2
+ * @author:刘飞跃
+ * @create:2019年03月04日18点57
+ */
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("大家好");
+    }
+}
